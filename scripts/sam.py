3d76d5d8877d9b8148170887c823880c9c6797f8
Nigel-NI <23294384+Nigel-NI@users.noreply.github.com>
2026-06-07 18:41:35 +0800

    Restore torch.load after SAM model load

--- scripts/sam.py
+++ scripts/sam.py
@@ -12,7 +12,7 @@ import json
 from modules import scripts, shared, script_callbacks
 from modules.ui import gr_show
 from modules.ui_components import FormRow, ToolButton
-from modules.safe import unsafe_torch_load, load
+from modules.safe import unsafe_torch_load
 from modules.processing import StableDiffusionProcessingImg2Img, StableDiffusionProcessing
 from modules.devices import device, torch_gc, cpu
 from modules.paths import models_path
@@ -87,12 +87,15 @@ def load_sam_model(sam_checkpoint):
     if 'hq' not in model_type and 'mobile' not in model_type:
         model_type = '_'.join(model_type.split('_')[:-1])
     sam_checkpoint_path = os.path.join(sam_model_dir, sam_checkpoint)
-    torch.load = unsafe_torch_load
-    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint_path)
-    sam.to(device=sam_device)
-    sam.eval()
-    torch.load = load
-    return sam
+    previous_torch_load = torch.load
+    try:
+        torch.load = unsafe_torch_load
+        sam = sam_model_registry[model_type](checkpoint=sam_checkpoint_path)
+        sam.to(device=sam_device)
+        sam.eval()
+        return sam
+    finally:
+        torch.load = previous_torch_load
 
 
 def clear_sam_cache():
